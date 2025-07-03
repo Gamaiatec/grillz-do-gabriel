797a967f6f60c6ca99f09f9e38fbee1509824fe7
Gamaiatec <gamaiatec@gmail.com>
2025-07-03 02:06:29 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GrillzCustomizer from './GrillzCustomizer.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GrillzCustomizer />
+  </React.StrictMode>
+)
