5e2daeab27b0dd193c6a73b100987684b30f5ced
Gamaiatec <gamaiatec@gmail.com>
2025-07-03 02:59:36 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom/client';
 import GrillzCustomizer from './GrillzCustomizer.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GrillzCustomizer />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<GrillzCustomizer />);
